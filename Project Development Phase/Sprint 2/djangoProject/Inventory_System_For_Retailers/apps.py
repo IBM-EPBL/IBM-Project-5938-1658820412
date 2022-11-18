5285ee9cf4bafc76cd73585fd542dab310bfa304
Danush <psdanush6900@gmail.com>
2022-11-19 00:00:34 +0530

    Added Sprint 2 Files

---
+++ Project Development Phase/Sprint 2/djangoProject/Inventory_System_For_Retailers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InventorySystemForRetailersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Inventory_System_For_Retailers'
